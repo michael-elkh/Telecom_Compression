c945cefe3fa702f33b7f5b6e47a754dc0c72828d
Michaël El Kharroubi <michael.elkh@gmail.com>
2018-04-22 14:37:16 +0200

    Optimisations ~7%, introduction d'un code à longueur variable.

--- Lempel-Ziv/Program.cs
+++ Lempel-Ziv/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
-using System.Diagnostics;
 
 namespace Lempel_Ziv
 {
 	class Program
 	{
 		const String FILE = "C:\\Users\\micha\\Desktop\\Test_Files\\TheSonnets.txt";
+		static Int32 Get_NbBits(Int32 value)
+		{
+			return (int)Math.Log(value, 2) + 1;
+		}
 		static List<Boolean> Get_List(int Value, int Nb_Bits = -1)
 		{
 			//Si le nombre bits n'est pas précisé, je le fixe au minimum.
@@ -43,30 +46,46 @@ namespace Lempel_Ziv
 		}
 		static List<Int32> Read(String Path)
 		{
-			Byte[] data = File.ReadAllBytes(Path);
-			Int32 nbbits = data[0];
-			List<Int32> input = new List<int>();
+			Byte[] input = File.ReadAllBytes(Path);
+			Int32 nbbits = input[0];
+			List<Int32> data = new List<int>();
 			List<Boolean> buffer = new List<bool>();
-			for (int i = 1; i < data.Length; i++)
+			for (int i = 1; i < input.Length; i++)
 			{
-				buffer.AddRange(Get_List(data[i], 8));
+				buffer.AddRange(Get_List(input[i], 8));
 				while (buffer.Count >= nbbits)
 				{
-					input.Add(Get_Int(buffer.GetRange(0, nbbits)));
+					int value = Get_Int(buffer.GetRange(0, nbbits));
 					buffer.RemoveRange(0, nbbits);
+					if (value == 0)
+					{
+						nbbits++;
+					}
+					else
+					{
+						data.Add(value);
+					}
 				}
 			}
 
-			return input;
+			return data;
 		}
-		static void Write(String Path, KeyValuePair<int, List<int>> input)
+		static void Write(String Path, List<int> input)
 		{
 			List<Byte> output = new List<Byte>();
 			List<Boolean> buffer = new List<bool>();
-			buffer.AddRange(Get_List(input.Key, 8));
-			for (int i = 0; i < input.Value.Count; i++)
+			int current = Get_NbBits(input[0]);
+			buffer.AddRange(Get_List(current, 8));
+			for (int i = 0; i < input.Count; i++)
 			{
-				buffer.AddRange(Get_List(input.Value[i], input.Key));
+				while(Get_NbBits(input[i]) > current)
+				{
+					//Séparateur
+					buffer.AddRange(Get_List(0, current));
+
+					current++;
+				}
+				buffer.AddRange(Get_List(input[i], current));
 				while (buffer.Count >= 8)
 				{
 					output.Add(Get_Byte(buffer.GetRange(0, 8)));
@@ -74,9 +93,9 @@ namespace Lempel_Ziv
 				}
 			}
 
-			if(buffer.Count > 0)
+			if (buffer.Count > 0)
 			{
-				while(buffer.Count < 8)
+				while (buffer.Count < 8)
 				{
 					buffer.Add(false);
 				}
@@ -85,21 +104,21 @@ namespace Lempel_Ziv
 			}
 			File.WriteAllBytes(Path, output.ToArray());
 		}
-		static KeyValuePair<int, List<Int32>> LZW_Compress(String Path)
+		static List<Int32> LZW_Compress(String Path)
 		{
 			//https://www.geeksforgeeks.org/lzw-lempel-ziv-welch-compression-technique/
 			List<int> output = new List<int>();
 			Byte[] input = File.ReadAllBytes(Path);
 			Dictionary<String, Int32> Table = new Dictionary<string, int>();
-			for (int i = 0; i < 256; i++)
+			for (int i = 1; i <= 256; i++)
 			{
-				Table.Add(((Char)i).ToString(), i);
+				Table.Add(((Char)(i-1)).ToString(), i);
 			}
 
 			string previous = ((Char)input[0]).ToString();
 			Char current;
 			string concat;
-			int cmpt = 255;
+			int cmpt = 256;
 			for (int i = 1; i < input.Length; i++)
 			{
 				current = (Char)input[i];
@@ -117,18 +136,16 @@ namespace Lempel_Ziv
 				}
 			}
 			output.Add(Table[previous]);
-			KeyValuePair<int, List<Int32>> res;
-			res = new KeyValuePair<int, List<int>>((int)Math.Log(output.Max(), 2) + 1, output);
-			return res;
+			return output;
 		}
 		static void LZW_Uncompress(List<Int32> input, String Path)
 		{
 			List<Char> output = new List<Char>();
 			Dictionary<Int32, String> Table = new Dictionary<int, String>();
 			int i;
-			for (i = 0; i < 256; i++)
+			for (i = 1; i <= 256; i++)
 			{
-				Table.Add(i, ((Char)i).ToString());
+				Table.Add(i, ((Char)(i-1)).ToString());
 			}
 
 			int old = input.First();
@@ -158,11 +175,38 @@ namespace Lempel_Ziv
 
 			File.WriteAllBytes(Path, Encoding.ASCII.GetBytes(output.ToArray()));
 		}
+		static void Analyse(List<int> Data)
+		{
+			List<int>[] bench = new List<int>[Get_NbBits(Data.Count)];
+			for (int i = 0; i < bench.Length; i++)
+			{
+				bench[i] = new List<int>();
+			}
+			int pos = 0;
+			int value = 0;
+			for (int i = 0; i < Data.Count; i++)
+			{
+				value = Math.Abs(Data[i]);
+				pos = (int)Math.Log(value, 2);
+				bench[pos].Add(value);
+			}
+			Double total = 0;
+			Double current = 0;
+			Console.WriteLine("Total : " + Data.Count.ToString("N0") + " - " + Math.Log(Data.Count, 2));
+			for (int i = 0; i < bench.Length; i++)
+			{
+				current = Math.Round((Double)bench[i].Count * 100 / Data.Count, 2);
+				total += current;
+				Console.WriteLine("Nb bits : " + (i + 1) + " - Pourcentage : " + current);
+			}
+			Console.WriteLine("Total : " + total);
+		}
 		static void Main(string[] args)
 		{
-			KeyValuePair<int, List<int>> compressed = LZW_Compress(FILE);
+			List<int> compressed = LZW_Compress(FILE);
 			Write(FILE + ".lzw", compressed);
-			compressed.Value.Clear();
+			compressed.Clear();
+
 			GC.Collect();
 			List<Int32> decompressed = Read(FILE + ".lzw");
 			LZW_Uncompress(decompressed, FILE.Substring(0, FILE.Length - 4) + ".original");
